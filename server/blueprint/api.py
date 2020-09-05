18faeff45bfa5d6ffdba930e2714401044e247c7
Lileso <jessica-rowley@outlook.com>
2020-09-05 14:58:20 +0100

    updating the API and working DB managment

--- server/blueprint/api.py
+++ server/blueprint/api.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from db_manager import database as db
+import datetime
 
 api = Blueprint('api', __name__)
 
 api.route('/api/<agent_name>/get_data')
-def get_data():
-    return "Data"
+def get_data(agent_name):
+    ''' This is an endpoint that the website can use to get and graph data.  
+    agent_name: This is a variable that is used to work out what agent is what in the DB file. Usually hostname of the agent.
+    '''
+    data = db.query_table(agent_name)
+    return jsonify(data)
 
-api.route('/api/<agent_name>/send_data')
-def send_data():
+api.route('/api/<agent_name>/send_data', method=["POST"])
+def send_data(agent_name):
+    ''' This is an endpoint that the agents use to sent their data. 
+    agent_name: This is a variable that is used to work out what agent is what in the DB file. Usually hostname of the agent.
+
+    Required JSON:
+    temperature : Value of recorded temp
+    humidity :  Value of recorded humidity
+    date_time : date and time of the sensor reading
+    '''
+    if not request.is_json:
+        return jsonify("exception_code": 400, "exception_message": "Invalid type, Please send JSON", "success": False), 400
+    sent_json = request.get_json()
+
     return "Done!"
--- server/blueprint/db_manager.py
+++ server/blueprint/db_manager.py
@@ -15,14 +15,14 @@ db_config = config['database']
 class database:
     def __init__(self):
         script_path = os.path.dirname(os.path.abspath(__file__))
-        self.engine = create_engine('sqlite:///'+script_path+'/dht_database.db', echo=True)
+        self.engine = create_engine('sqlite:///'+script_path+'/dht_database.db')
         self.metadata = MetaData(bind=self.engine)
         self.metadata.create_all()
         self.metadata.reflect(bind=self.engine)
 
     def create_agent(self, agent_name):
         if not self.engine.dialect.has_table(self.engine, agent_name):
-            agent_table = Table(agent_name, self.metadata, Column('id', Integer, primary_key=True, autoincrement=True), Column('humidity', Integer), Column('temperature',Integer), Column('date_time', Date), autoload=True)
+            agent_table = Table(agent_name, self.metadata, Column('id', Integer, primary_key=True, autoincrement=True), Column('humidity', Integer), Column('temperature',Integer), Column('date_time', Date))
             agent_table.create(bind=self.engine)
             self.metadata.reflect(bind=self.engine)
 
@@ -46,7 +46,8 @@ class database:
             humidity_trace = dict(x=date_list, y=humidity_list, name='Humidity (%)', line=dict(width=7, color= "blue"))
             data= [humidity_trace,temperature_trace]
             current_temp = [temperature_list[0]]
-            return data, current_temp
+            return {"Success":True, "data":data, "current_temp":current_temp}
+        return{"Success": False}
     
     def remove_agent(self, agent_name):
         agent_table = self.metadata.tables.get(agent_name)
@@ -58,6 +59,10 @@ if __name__ == "__main__":
     db.add_record(agent_name="Test_agent", humidity=1, temperature=2, date_time=datetime.datetime.now())
     results = db.query_table("Test_agent")
     print(results)
-    print("Running Test for if nothing in DB")
+    print("\nRunning Test for if nothing in DB")
     db.create_agent("Test_agent")
     print(db.query_table("Test_agent2"))
+    print("\nRunning Test for dropping table")
+    db.remove_agent("Test_agent")
+    print("\nRunning Test for dropping a table that doesn't exist")
+    db.remove_agent("Test_agent2")
