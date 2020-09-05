b893a2652fe837f8d1d1599bc9a4f6f4fd5f7465
Lileso <jessica-rowley@outlook.com>
2020-09-05 14:24:07 +0100

    API and Dabase manager

--- server/blueprint/api.py
+++ server/blueprint/api.py
@@ -1,3 +1,11 @@
 from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__)
+
+api.route('/api/<agent_name>/get_data')
+def get_data():
+    return "Data"
+
+api.route('/api/<agent_name>/send_data')
+def send_data():
+    return "Done!"
